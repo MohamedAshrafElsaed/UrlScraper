4cc74ee894dfb891461c9303d1d51ee7b8e88f48
Mohamed Elsaed <m.ashraf@converted.in>
2023-11-22 22:46:07 +0200

    add cash

--- main.go
+++ main.go
@@ -4,20 +4,45 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"golang.org/x/net/html"
+	"io"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+type CacheItem struct {
+	Response *MetaTagsResponse
+	Count    int
+}
+
+var (
+	client = &http.Client{
+		Timeout: 5 * time.Second,
+	}
+	cache = make(map[string]*CacheItem)
+	mu    sync.RWMutex
 )
 
 func main() {
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/meta", handleRequest).Methods("POST")
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+type MetaTagsResponse struct {
+	URL  string `json:"url"`
+	Data struct {
+		Description string            `json:"description"`
+		OgTags      map[string]string `json:"ogTags"`
+	} `json:"data"`
+	Error string `json:"error,omitempty"`
+}
+
 type request struct {
-	URL string `json:"url"`
+	URLs []string `json:"urls"`
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
@@ -28,37 +53,76 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tags, err := getMetaTags(req.URL)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	responses := make([]*MetaTagsResponse, len(req.URLs))
+	var wg sync.WaitGroup
+	for i, url := range req.URLs {
+		wg.Add(1)
+		go func(i int, url string) {
+			defer wg.Done()
+			tags, err := getMetaTags(url)
+			if err != nil {
+				log.Printf("Error fetching meta tags for URL %s: %v", url, err)
+				responses[i] = &MetaTagsResponse{URL: url, Error: err.Error()}
+				return
+			}
+			responses[i] = tags
+		}(i, url)
 	}
+	wg.Wait()
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tags)
-}
-
-type MetaTagsResponse struct {
-	Data struct {
-		Description   string `json:"description"`
-		OgDescription string `json:"ogDescription"`
-		OgTitle       string `json:"ogTitle"`
-	} `json:"data"`
+	err = json.NewEncoder(w).Encode(responses)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 }
 
 func getMetaTags(url string) (*MetaTagsResponse, error) {
-	resp, err := http.Get(url)
+	mu.Lock()
+	item, found := cache[url]
+	if found {
+		item.Count++
+		if item.Count >= 2 {
+			// Invalidate the cache for this URL
+			delete(cache, url)
+		} else {
+			cache[url] = item // Update the cache with the incremented count
+		}
+		mu.Unlock()
+		if item.Count < 2 {
+			return item.Response, nil
+		}
+		// If the count is 2, we proceed to fetch the data again
+	} else {
+		mu.Unlock()
+	}
+
+	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	tagsResponse := &MetaTagsResponse{}
+	tagsResponse := &MetaTagsResponse{
+		URL: url,
+		Data: struct {
+			Description string            `json:"description"`
+			OgTags      map[string]string `json:"ogTags"`
+		}{
+			OgTags: make(map[string]string),
+		},
+	}
 	var f func(*html.Node)
 	f = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "meta" {
@@ -70,20 +134,28 @@ func getMetaTags(url string) (*MetaTagsResponse, error) {
 					value = a.Val
 				}
 			}
-			switch key {
-			case "description":
+			if strings.HasPrefix(key, "og:") {
+				camelCaseKey := toCamelCase(key)
+				tagsResponse.Data.OgTags[camelCaseKey] = value
+			} else if key == "description" {
 				tagsResponse.Data.Description = value
-			case "og:description":
-				tagsResponse.Data.OgDescription = value
-			case "og:title":
-				tagsResponse.Data.OgTitle = value
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
 			f(c)
 		}
 	}
-
 	f(doc)
+	mu.Lock()
+	cache[url] = &CacheItem{Response: tagsResponse, Count: 1}
+	mu.Unlock()
 	return tagsResponse, nil
+}
+
+func toCamelCase(str string) string {
+	parts := strings.Split(str, ":")
+	for i := 1; i < len(parts); i++ {
+		parts[i] = strings.Title(parts[i])
+	}
+	return strings.Join(parts, "")
 }
